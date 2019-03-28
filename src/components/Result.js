72632001481ca7907b829d59f8506988836418ad
Samundra Khatri <Samundrak@yahoo.com>
2019-03-28 10:22:50 +0545

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import ankah from 'ankah';
 import CountUp from 'react-countup';
 import LanguageContext from '../contexts/LanguageContext';
 import SpeedDistance from './SpeedDistance';
